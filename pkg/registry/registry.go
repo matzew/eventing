ac212454f0c2fce1328a814b5d1d94db5cf857f3
Matthias Wessendorf <mwessend@redhat.com>
2023-03-24 13:08:03 +0100

    Updates for CE Regsitry compliant endpoint
    Signed-off-by: Matthias Wessendorf <mwessend@redhat.com>

--- pkg/registry/registry.go
+++ pkg/registry/registry.go
@@ -9,15 +9,34 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"knative.dev/eventing/pkg/apis/eventing/v1beta1"
 	eventingclientset "knative.dev/eventing/pkg/client/clientset/versioned"
+	"knative.dev/pkg/apis"
 	"strings"
 
 	"log"
 )
 
-type payload struct {
-	Sourcename  string   `json:"sourcename"`
+type Group struct {
+	ID          string       `json:"id"`
+	Version     int          `json:"version"`
+	Definitions []Definition `json:"definitions"`
+}
+
+type Definition struct {
+	ID          string   `json:"id"`
+	Version     int      `json:"version"`
 	Description string   `json:"description"`
-	Events      []string `json:"events"`
+	SchemaURL   string   `json:"schemaUrl"`
+	Format      string   `json:"format"`
+	Metadata    Metadata `json:"metadata"`
+}
+
+type Metadata struct {
+	Attributes map[string]Attribute `json:"attributes"`
+}
+
+type Attribute struct {
+	Required bool        `json:"required"`
+	Value    interface{} `json:"value,omitempty"`
 }
 
 // Handler is the HTTP handler for the registry.
@@ -46,7 +65,7 @@ func (h *Handler) Start(ctx context.Context) error {
 }
 
 func (h *Handler) receive(ctx context.Context, event cloudevents.Event) (*cloudevents.Event, cloudevents.Result) {
-	data := &payload{}
+	data := &Group{}
 	if err := event.DataAs(data); err != nil {
 		log.Printf("Got error while unmarshalling data: %s", err.Error())
 		return nil, http.NewResult(400, "got error while unmarshalling data: %w", err)
@@ -55,19 +74,19 @@ func (h *Handler) receive(ctx context.Context, event cloudevents.Event) (*cloude
 	//TODO fix me: we should not hardcode the namespace
 	systemNamespace := "knative-eventing"
 
-	for _, event := range data.Events {
+	for _, definition := range data.Definitions {
+
 		et := &v1beta1.EventType{
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      strings.ToLower(data.Sourcename) + "-" + strings.ToLower(event),
+				Name:      strings.ToLower(data.ID) + "-" + strings.ToLower(definition.ID),
 				Namespace: systemNamespace,
 			},
 			Spec: v1beta1.EventTypeSpec{
-				Type:        event,
-				Source:      nil,
-				Schema:      nil,
-				SchemaData:  "",
+				Type:        definition.ID,
+				Source:      readURLValue(definition.Metadata.Attributes["source"]),
+				Schema:      readURLValue(definition.Metadata.Attributes["dataschema"]),
 				Broker:      "",
-				Description: data.Description,
+				Description: definition.Description,
 			},
 		}
 		_, err := h.eventingClient.EventingV1beta1().EventTypes(systemNamespace).Create(ctx, et, metav1.CreateOptions{})
@@ -76,4 +95,12 @@ func (h *Handler) receive(ctx context.Context, event cloudevents.Event) (*cloude
 		}
 	}
 	return nil, nil
+}
+
+func readURLValue(attr Attribute) *apis.URL {
+	url, err := apis.ParseURL(attr.Value.(string))
+	if err != nil {
+		return nil
+	}
+	return url
 }
